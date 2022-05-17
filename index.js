950c8e1f2c48c93834903a33237b0e9aeb3e8c2e
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-16 21:28:28 -0400

    commit tree 3bc783e031054bc3ea78ea0fa88963a97721ca13

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if ( distance <= 400){
+    const result = 'This one is on me!';
+    return result
+    }
+  
+   if ( distance > 400 && distance<= 2000){
+    
+     const result = 'That will be twenty bucks.';
+    return result
+    }
+  
+   if ( distance > 2000 && distance <= 2500){
+    const result = 'I will gladly take your thirty bucks.';
+    return result
+     
+    }
+  
+   if ( distance > 2500){
+     const result = 'No can do.';
+    return result
+    }
+  
+  }
+
+  scuberGreetingForFeet()
+
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    const shouldiStayOrShouldIGo = 'Ok, sounds good.';
+    return shouldiStayOrShouldIGo
+  }
+  else {
+    const shouldiStayOrShouldIGo = 'No go.';
+    return shouldiStayOrShouldIGo
+    
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+if ( tip === 'generous') {
+  
+  console.log('Thank you so much.');
+  return 'Thank you so much.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+if (tip === 'not as generous') {
+  
+  console.log('Thank you.');
+  return 'Thank you.'
+  
 }
+
+else {  
+  console.log('Bye.');
+  return 'Bye.'
+  
+}
+
+}
+switchOnCharmFromTip()
